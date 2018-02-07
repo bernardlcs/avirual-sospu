caff984adc50211853830bc66abf17f690f31f4b
bernardlcs <bernard.lenin4@gmail.com>
2018-02-07 15:16:18 -0200

    commit do 2 deploy

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,16 @@
-import { AppPage } from './app.po';
+import { Angular4ClientPage } from './app.po';
 
-describe('sospu App', () => {
-  let page: AppPage;
+describe('angular4-client App', () => {
+  let page: Angular4ClientPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new Angular4ClientPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display welcome message', done => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
+export class Angular4ClientPage {
   navigateTo() {
     return browser.get('/');
   }
